e691c82d1c7a5aae84b7048b44af7d18817d7fe5
sherKuo <sherilynkuo@gmail.com>
2024-05-29 14:00:03 -0700

    running into an error

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
- 
-import Link from 'next/link';
+import React from 'react';
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1 className='text-3xl'>Welcome</h1>
-      <Link href='/properties'>Show Properties</Link>
-    </div>
-  )
-}
+    return (
+        <>
+            <Hero />
+            <InfoBoxes />
 
-export default HomePage
+        </>
+    );
+};
+
+export default HomePage;
