cd545c488e929f7a4268c8b6263b37fd959324c2
sherKuo <sherilynkuo@gmail.com>
2024-05-29 12:22:22 -0700

    navbar has been updated but not yet completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
 import '@/assets/styles/global.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
     title: 'PropertyPulse | Find The Perfect Rental',
     description: 'Find your dream rental property',
-    keywords:'rental, find rentals, find properties'
-}
+    keywords: 'rental, find rentals, find properties',
+};
 
-const MainLayout = ({ children}) => {
-  return (
-    < html lang='en'>
-        <body>
-            <div>{ children }</div>
-        </body>
-    </html>
-    
-  )
-}
+const MainLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body>
+                <Navbar />
+                <main>{children}</main>
+            </body>
+        </html>
+    );
+};
 
-export default MainLayout
+export default MainLayout;
