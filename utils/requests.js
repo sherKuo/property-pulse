2c2de97c512ff9034372aa08e44d0d1fb537cab7
sherKuo <sherilynkuo@gmail.com>
2024-06-17 14:58:37 -0700

    new routes created

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch all properties
+
 async function fetchProperties() {
     try {
         // Handle the case where the domain is not available yet
@@ -16,6 +18,29 @@ async function fetchProperties() {
     } catch (error) {
         console.log(error);
         return [];
+    }
+}
+
+export { fetchProperty };
+
+// Fetch single property
+
+async function fetchProperty(id) {
+    try {
+        // Handle the case where the domain is not available yet
+        if (!apiDomain) {
+            return null;
+        }
+
+        const res = await fetch(`${apiDomain}/properties/${id}`);
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        return res.json();
+    } catch (error) {
+        console.log(error);
+        return null;
     }
 }
 
