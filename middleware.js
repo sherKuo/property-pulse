8cb50557c65bf58615a5800a85b13a5a053c1a4f
Jefferson Chen <me@chenjefferson.com>
2024-06-30 12:52:11 -0700

    add changes -- add missing userId and stop trying to use multipart form data

--- middleware.js
+++ middleware.js
@@ -1,5 +1,8 @@
 export { default } from 'next-auth/middleware';
 
 export const config ={
-    matcher:['/properties/add', '/profile', '/properties/saved/messages']
+    matcher:['/properties/add', '/profile', '/properties/saved/messages'],
+    api: {
+        bodyParser: true,
+    }
 };
