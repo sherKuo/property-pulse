01b375ec2b097b5faeef94bc1bd671388ae10cca
Sherilyn Kuo <sherilynkuo@gmail.com>
2024-07-10 23:10:07 -0700

    temp changes to footer Need to come back to fix formatting.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '@/assets/images/logo.png';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
@@ -10,11 +12,22 @@ const Footer = () => {
         <footer className='bg-gray-200 py-4 mt-24'>
             <div className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'>
                 <div className='mb-4 md:mb-0'>
+                    <p className='justify-center text-sm text-gray-500 mt-2 md:mt-0'>
+                        This is a work in progress! I subscribe to the CI/CD lifestyle and you can find more up to date versions on my github.
+                    </p>
+                </div>
+                    
+            </div>
+            <div className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'>
+                <div className='mb-4 md:mb-0'>
                     <Image
                         src={logo}
                         alt='Logo'
                         className='h-8 w-auto'
                     />
+                    <Link href={'https://github.com/sherKuo/property-pulse'}><FaGithub className='inline mr-2'></FaGithub>
+                    </Link>
+                    
                 </div>
 
                 <div>
