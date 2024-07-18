7f42adc53eb1443553d3b1ca878482cfa7b4770b
Sherilyn Kuo <sherilynkuo@gmail.com>
2024-07-17 19:28:44 -0700

    Bookmarking capabilities are complete!

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -5,6 +5,29 @@ import { getSessionUser } from '@/utils/getSessionUser';
 
 export const dynamic = 'force-dynamic';
 
+// GET /api/bookmarks
+export const GET = async () => {
+	try {
+		await connectDB();
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID is required', { status: 401 });
+		}
+
+		// Find user in database
+		const user = await User.findOne({ _id: sessionUser.userId });
+
+		// Get users bookmarks by finding property ids that are in the array of user.bookmarks
+		const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+		return new Response(JSON.stringify(bookmarks), { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return new Response('Something went wrong.', { status: 500 });
+	}
+};
+
 export const POST = async (request) => {
 	try {
 		await connectDB();
--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -8,85 +8,94 @@ import Image from 'next/image';
 import pin from '@/assets/images/pin.svg';
 
 const PropertyMap = ({ property }) => {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 0,
-    longitude: 0,
-    zoom: 12,
-    width: '100%',
-    height: '500px',
-  });
-  const [loading, setLoading] = useState(true);
-  const [geocodeError, setGeocodeError] = useState(false);
+	const [lat, setLat] = useState(null);
+	const [lng, setLng] = useState(null);
+	const [viewport, setViewport] = useState({
+		latitude: 0,
+		longitude: 0,
+		zoom: 12,
+		width: '100%',
+		height: '500px',
+	});
+	const [loading, setLoading] = useState(true);
+	const [geocodeError, setGeocodeError] = useState(false);
 
-  setDefaults({
-    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
-    language: 'en',
-    region: 'us',
-  });
+	setDefaults({
+		key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+		language: 'en',
+		region: 'us',
+	});
 
-  useEffect(() => {
-    const fetchCoords = async () => {
-      try {
-        const res = await fromAddress(
-          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
-        );
+	useEffect(() => {
+		const fetchCoords = async () => {
+			try {
+				const res = await fromAddress(
+					`${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+				);
 
-        //  Check for results
-        if (res.results.length === 0) {
-          // No results found
-          setGeocodeError(true);
-          setLoading(false);
-          return;
-        }
+				//  Check for results
+				if (res.results.length === 0) {
+					// No results found
+					setGeocodeError(true);
+					setLoading(false);
+					return;
+				}
 
-        const { lat, lng } = res.results[0].geometry.location;
+				const { lat, lng } = res.results[0].geometry.location;
 
-        setLat(lat);
-        setLng(lng);
-        setViewport({
-          ...viewport,
-          latitude: lat,
-          longitude: lng,
-        });
+				setLat(lat);
+				setLng(lng);
+				setViewport({
+					...viewport,
+					latitude: lat,
+					longitude: lng,
+				});
 
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setGeocodeError(true);
-        setLoading(false);
-      }
-    };
+				setLoading(false);
+			} catch (error) {
+				console.log(error);
+				setGeocodeError(true);
+				setLoading(false);
+			}
+		};
 
-    fetchCoords();
-  }, []);
+		fetchCoords();
+	}, []);
 
-  if (loading) return <Spinner loading={loading} />;
+	if (loading) return <Spinner loading={loading} />;
 
-  // Handle case where geocoding failed
-  if (geocodeError) {
-    return <div className='text-xl'>No location data found</div>;
-  }
+	// Handle case where geocoding failed
+	if (geocodeError) {
+		return <div className='text-xl'>No location data found</div>;
+	}
 
-  return (
-    !loading && (
-      <Map
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-        mapLib={import('mapbox-gl')}
-        initialViewState={{
-          longitude: lng,
-          latitude: lat,
-          zoom: 15,
-        }}
-        style={{ width: '100%', height: 500 }}
-        mapStyle='mapbox://styles/mapbox/streets-v9'
-      >
-        <Marker longitude={lng} latitude={lat} anchor='bottom'>
-          <Image src={pin} alt='location' width={40} height={40} />
-        </Marker>
-      </Map>
-    )
-  );
+	return (
+		!loading && (
+			<Map
+				mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+				mapLib={import('mapbox-gl')}
+				initialViewState={{
+					longitude: lng,
+					latitude: lat,
+					zoom: 15,
+				}}
+				style={{ width: '100%', height: 500 }}
+				mapStyle='mapbox://styles/mapbox/streets-v9'
+			>
+				<Marker
+					longitude={lng}
+					latitude={lat}
+					anchor='bottom'
+				>
+					<Image
+						src={pin}
+						alt='location'
+						width={40}
+						height={40}
+					/>
+				</Marker>
+			</Map>
+		)
+	);
 };
 export default PropertyMap;
